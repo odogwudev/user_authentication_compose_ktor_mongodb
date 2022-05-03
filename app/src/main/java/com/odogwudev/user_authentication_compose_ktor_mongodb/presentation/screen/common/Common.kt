ad35bd55552ba63cde6f9613100ff1af226ec9a6
Michael Enoma <odogwudev@e.email>
2022-05-03 02:25:33 +0100

    One Tap Signin/SignUp

---
+++ app/src/main/java/com/odogwudev/user_authentication_compose_ktor_mongodb/presentation/screen/common/Common.kt
@@ -0,0 +1,91 @@
+package com.odogwudev.user_authentication_compose_ktor_mongodb.presentation.screen.common
+
+import android.app.Activity
+import android.util.Log
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.CommonStatusCodes
+import com.odogwudev.user_authentication_compose_ktor_mongodb.util.Constants.CLIENT_ID
+
+
+fun signIn(
+    activity: Activity,
+    launchActivityResult: (IntentSenderRequest) -> Unit,
+    accountNotFound: () -> Unit
+) {
+    val oneTapClient = Identity.getSignInClient(activity)
+    val signInRequest = BeginSignInRequest.builder()
+        .setGoogleIdTokenRequestOptions(
+            BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                .setSupported(true)
+                .setServerClientId(CLIENT_ID)
+                .setFilterByAuthorizedAccounts(true)
+                .build()
+        )
+        .setAutoSelectEnabled(true)//allows you to sign into one tap without need for tapping continue
+        .build()
+
+    oneTapClient.beginSignIn(signInRequest)
+        .addOnSuccessListener { result ->
+            try {
+                launchActivityResult(
+                    IntentSenderRequest.Builder(
+                        result.pendingIntent.intentSender
+                    ).build()
+                )
+            } catch (e: Exception) {
+                Log.d("SignIn", "Couldn't start One Tap UI: ${e.message}")
+            }
+        }
+        .addOnFailureListener {
+            Log.d("SignIn", "Signing Up...")
+            signUp(
+                activity = activity,
+                launchActivityResult = launchActivityResult,
+                accountNotFound = accountNotFound
+            )
+        }
+}
+
+fun signUp(
+    activity: Activity,
+    launchActivityResult: (IntentSenderRequest) -> Unit,
+    accountNotFound: () -> Unit
+) {
+    val oneTapClient = Identity.getSignInClient(activity)
+    val signInRequest = BeginSignInRequest.builder()
+        .setGoogleIdTokenRequestOptions(
+            BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                .setSupported(true)
+                .setServerClientId(CLIENT_ID)
+                .setFilterByAuthorizedAccounts(false)
+                .build()
+        )
+        .build()
+
+    oneTapClient.beginSignIn(signInRequest)
+        .addOnSuccessListener { result ->
+            try {
+                launchActivityResult(
+                    IntentSenderRequest.Builder(
+                        result.pendingIntent.intentSender
+                    ).build()
+                )
+            } catch (e: Exception) {
+                Log.d("SignUp", "Couldn't start One Tap UI: ${e.message}")
+            }
+        }
+        .addOnFailureListener {
+            Log.d("SignUp", "${it.message}")
+            accountNotFound()
+        }
+}
--- app/src/main/java/com/odogwudev/user_authentication_compose_ktor_mongodb/util/Constants.kt
+++ app/src/main/java/com/odogwudev/user_authentication_compose_ktor_mongodb/util/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
         "http://10.0.2.2:8080"// i didnt use 127 because i am using emulator to test this
     const val PREFERENCES_SIGNED_IN_KEY = "signed_in_key"
     const val PREFERENCES_NAME = "app_preferences"
+    const val CLIENT_ID = "608553142506-ftuh8luu6j2na5hbl48oehfdjlg18noc.apps.googleusercontent.com"
 }
